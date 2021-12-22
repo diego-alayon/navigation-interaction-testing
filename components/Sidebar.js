6c75ca546526ffddab0b560a23ec2da2be84495e
Diego Alayon Gomez <30353719+diego-alayon@users.noreply.github.com>
2021-12-22 11:50:40 +0100

    Created Sidebar component

---
+++ components/Sidebar.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar'
+import { UilAngleDoubleLeft } from '@iconscout/react-unicons'
+import { Sites, Boards, Scaning, Inventory, Software, Reports, Configuration } from '../bbdd/items-navigation'
+
+const Sidebar = ({collapsed}) => {
+
+    const [toggle, setToggle] = useState(collapsed)
+    
+
+    return (
+        <>
+            <ProSidebar collapsed={toggle ? true : false}>
+            <Menu iconShape="square">
+                <MenuItem icon={<UilAngleDoubleLeft onClick={() => setToggle(!toggle)} />}>Lansweeper</MenuItem>
+                {console.log('Sites',Sites)}
+                <MenuItem>Link-1</MenuItem>
+                    <SubMenu title="Sites">
+                        {Sites?.map(Sites => <MenuItem>{Sites}</MenuItem>)}
+                    </SubMenu>
+            </Menu>
+            </ProSidebar>;
+        </>
+    )
+}
+
+export default Sidebar
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,10 @@
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
+import Sidebar from '../components/Sidebar';
 
 export default function Home() {
   return (
     <>
-<ProSidebar>
-  <Menu iconShape="square">
-    <MenuItem>Dashboard</MenuItem>
-    <SubMenu title="Components">
-      <MenuItem>Component 1</MenuItem>
-      <MenuItem>Component 2</MenuItem>
-    </SubMenu>
-  </Menu>
-</ProSidebar>;
+      <Sidebar />
     </>
   )
 }
