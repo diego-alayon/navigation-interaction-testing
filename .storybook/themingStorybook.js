3f2e848bca28ae5e3047799b90dc64ab127aafb1
Diego Alayon Gomez <30353719+diego-alayon@users.noreply.github.com>
2021-12-28 20:27:21 +0100

    configuration tailwind in storybook

--- .storybook/themingStorybook.js
+++ .storybook/themingStorybook.js
@@ -2,7 +2,7 @@ import { create } from '@storybook/theming';
 
 export default create({
   base: 'light',
-  brandTitle: 'Lansweeper Storybook',
-  brandUrl: 'https://example.com',
-  brandImage: 'http://localhost:6006/assets/imgs/Lansweeper-Full-Logo.png',
+  brandTitle: 'Lansweeper',
+  brandUrl: 'https://example.com'
+  // brandImage: 'http://localhost:6006/assets/imgs/Lansweeper-Full-Logo.png',
 });
