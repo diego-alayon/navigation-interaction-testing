b56f3ee6b5134bf02f1520dab11e2503ad009857
Diego Alayon Gomez <30353719+diego-alayon@users.noreply.github.com>
2022-01-13 22:32:21 +0100

    Render icon as a prop in component parent

--- components/AccordionMenu.js
+++ components/AccordionMenu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Chevron from '../assets/Icons/chevron'
+import Icon from './Icon'
 
 const AccordionMenu = ({ label, id, href, children, icon }) => {
     return (
@@ -7,7 +8,7 @@ const AccordionMenu = ({ label, id, href, children, icon }) => {
             <div className="mainMenu">
                 <div className="item cursor-pointer" id={id}>
                     <div className='flex h-8'>
-                    <div>{icon}</div>
+                    <Icon icon={icon} />
                         <Chevron width={6} className='mr-4' />
                         <a href={href} className="btn mt-1">{label}</a>
                     </div>
--- components/NavigationSystem.js
+++ components/NavigationSystem.js
@@ -9,13 +9,13 @@ const NavigationSystem = () => {
         <div className='navigationSystem'>
             <div className='header px-4'>Gapac site</div>
             <div className='px-4'>
-                <AccordionMenu label="boards" id="boards" href="#boards">
+                <AccordionMenu label="boards" id="boards" href="#boards" icon='info'>
                     {ItemsNavigation?.map(ItemsNavigation => <>{ItemsNavigation?.boards?.map(nodes => <div>{nodes}</div>)}</>)}
                     <Toggle subitem="prueba">
                         Second level
                     </Toggle>
                 </AccordionMenu>
-                <AccordionMenu label="item" id="item" href="#item">
+                <AccordionMenu label="item" id="item" href="#item" icon='warning'>
                     {ItemsNavigation?.map(ItemsNavigation => <>{ItemsNavigation?.boards?.map(nodes => <div>{nodes}</div>)}</>)}
                 </AccordionMenu>
             </div>
